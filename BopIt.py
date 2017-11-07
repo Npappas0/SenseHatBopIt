a0b0d962a492288502c8cfd164e615fc2f3bc626
Npappas0 <nickdpap@gmail.com>
2017-11-07 18:28:31 +0000

    Edited BopIt

--- BopIt.py
+++ BopIt.py
@@ -1,5 +1,16 @@
 from Gyro import gyro
+from sense_hat import SenseHat
+from random import randint
 g = gyro()
+s = SenseHat()
+myList = ["Rotate It", "Flick It", "Flip It"]
+s.show_message("H")
+randNum = randint(0,2)
+print(myList[randNum])
+
+if(randNum == 0):
+    
+
 g.update()
 print(g.getRoll())
 print(g.getPitch())
