8ece2effa02a0891f8e18ea54920f196f8c3c84a
Npappas0 <nickdpap@gmail.com>
2017-11-21 18:52:18 +0000

    Finished project

--- BopIt.py
+++ BopIt.py
@@ -1,18 +1,40 @@
 from Gyro import gyro
+from score import score
 from sense_hat import SenseHat
 from random import randint
+import time
+sh = SenseHat()
+sh.set_rotation(180)
 g = gyro()
-s = SenseHat()
-myList = ["Rotate It", "Flick It", "Flip It"]
-s.show_message("H")
-randNum = randint(0,2)
-print(myList[randNum])
+s = score("PlayerOne")
+myList = ["Flip it", "Flick it", "Shake it"]
+sh.show_message("Welcome to BopIt", scroll_speed=0.06, text_colour=[0,255,255])
+time.sleep(1)
+sh.show_message("Ready? Lets Go", scroll_speed=0.06, text_colour=[0,255,255])
+gameOver = False
 
-if(randNum == 0):
-    
+def command(n, g, gameOver):
+    time.sleep(1)
+    sh.show_message(myList[n], scroll_speed=0.03)
+    k = 10
+    while(k > 0 and gameOver == False):
+        time.sleep(0.25)
+        k -= 1
+        if(n == 0 and g.flipped()):
+            s.changeScore(1)
+            gameOver = command(randint(0,2), g, gameOver)
+        elif(n == 1):
+            for event in sh.stick.get_events():
+                if(event.action == 'pressed'):
+                    s.changeScore(1)
+                    gameOver = command(randint(0,2), g, gameOver)
+        elif(n == 2 and g.shook()):
+            s.changeScore(1)
+            gameOver = command(randint(0,2), g, gameOver)
+    if(gameOver == False):
+        print("GameOver Your score was ")
+        print(s.showScore())
+        return True
 
-g.update()
-print(g.getRoll())
-print(g.getPitch())
-print(g.getYaw())
+command(randint(0,2), g, gameOver)
 
--- Gyro.py
+++ Gyro.py
@@ -4,14 +4,21 @@ class gyro:
         self.roll = 0
         self.pitch = 0
         self.yaw = 0
+        self.aroll = 0
+        self.apitch = 0
+        self.ayaw = 0
 
     def update(self):
         sense = SenseHat()
         sense.clear()
         o = sense.get_orientation()
+        a = sense.get_accelerometer()
         self.pitch = o["pitch"]
         self.roll = o["roll"]
         self.yaw = o["yaw"]
+        self.apitch = a["pitch"]
+        self.aroll = a["roll"]
+        self.ayaw = a["yaw"]
         
     def getRoll(self):
         return self.roll
@@ -21,3 +28,22 @@ class gyro:
 
     def getYaw(self):
         return self.yaw
+
+    def getaRoll(self):
+        return self.aroll
+
+    def getaPitch(self):
+        return self.apitch
+
+    def getaYaw(self):
+        return self.ayaw
+
+    def flipped(self):
+        self.update()
+        if(self.getRoll() > 160 and self.getRoll() < 200):
+            return True
+
+    def shook(self):
+        self.update()
+        if(self.getaYaw() > 160 and self.getaYaw() < 200):
+            return True
